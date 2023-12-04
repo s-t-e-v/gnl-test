fc6b58979c4a21097a0deb09fb4395976b332769
Steven Bandaogo <sbandaog@student.42.fr>
2023-12-04 14:19:37 +0100

    initial commit

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "get_next_line.h"
+
+int main(void)
+{
+    printf("%s\n", get_next_line(1));
+    return (0);
+}
---
+++ test.sh
@@ -0,0 +1,2 @@
+cc -Wall -Wextra -Werror -g ../get_next_line.c ../get_next_line_utils.c test.c -o tests -I../
+./tests
