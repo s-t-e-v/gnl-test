c1a34ca90774a62754e14d4c799063dbcc46d034
Steven Bandaogo <sbandaog@student.42.fr>
2023-12-04 18:19:43 +0100

    update test.c

--- test.c
+++ test.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "get_next_line.h"
 
 int main(void)
 {
-    printf("%s\n", get_next_line(1));
+    int		fd;
+
+    fd = open("test1.txt", 00);
+
+    if (fd < 0)
+        return (printf("Error opening file!\n"), 1);
+    printf("%s\n", get_next_line(fd));
+    close(fd);
     return (0);
 }
