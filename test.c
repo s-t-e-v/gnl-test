b3448079e8263af48e2149a807c577411b9f1cab
Steven Bandaogo <sbandaog@student.42.fr>
2023-12-06 16:19:08 +0100

    add OK/KO + number of tests passed

--- test.c
+++ test.c
@@ -2,31 +2,55 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "get_next_line.h"
+#include <bsd/string.h>
+
+static int  testpassed = 0;
+static int  nb_test = 0;
 
 static int test6()
 {
     int		fd;
     char    *line;
+    int     ntest = 0;
+    int     passed = 0;
 
-    printf("\n> test 6\n");
+    nb_test++;
+    printf("\n> test 6");
 
     fd = open("test6.txt", 00);
 
     if (fd < 0)
         return (printf("Error opening file!\n"), -1);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "Hello World");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
     if (line)
         free(line);
     close(fd);
+
+    if (ntest == passed)
+    {
+        printf(" - OK");
+        testpassed++;
+    }
+    else
+        printf(" - KO");
     return (1);
 }
 
@@ -34,26 +58,46 @@ static int test5()
 {
     int		fd;
     char    *line;
+    int     ntest = 0;
+    int     passed = 0;
 
-    printf("\n> test 5\n");
+    nb_test++;
+    printf("\n> test 5");
 
     fd = open("test5.txt", 00);
 
     if (fd < 0)
         return (printf("Error opening file!\n"), -1);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "Bonjour");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
     if (line)
         free(line);
     close(fd);
+
+    if (ntest == passed)
+    {
+        printf(" - OK");
+        testpassed++;
+    }
+    else
+        printf(" - KO");
     return (1);
 }
 
@@ -61,26 +105,46 @@ static int test4()
 {
     int		fd;
     char    *line;
+    int     ntest = 0;
+    int     passed = 0;
 
-    printf("\n> test 4\n");
+    nb_test++;
+    printf("\n> test 4");
 
     fd = open("test4.txt", 00);
 
     if (fd < 0)
         return (printf("Error opening file!\n"), -1);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "a\n");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
     if (line)
         free(line);
     close(fd);
+
+    if (ntest == passed)
+    {
+        printf(" - OK");
+        testpassed++;
+    }
+    else
+        printf(" - KO");
     return (1);
 }
 
@@ -88,53 +152,47 @@ static int test3()
 {
     int		fd;
     char    *line;
+    int     ntest = 0;
+    int     passed = 0;
 
-    printf("\n> test 3\n");
+    nb_test++;
+    printf("\n> test 3");
 
     fd = open("test3.txt", 00);
 
     if (fd < 0)
         return (printf("Error opening file!\n"), -1);
+
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "\n");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
     if (line)
         free(line);
     close(fd);
-    return (1);
-}
 
-static int test2()
-{
-    int		fd;
-    char    *line;
-
-    printf("\n> test 2\n");
-
-    fd = open("test2.txt", 00);
-
-    if (fd < 0)
-        return (printf("Error opening file!\n"), -1);
-    line =  get_next_line(fd);
-    printf("%s", line);
-    if (line)
-        free(line);
-    line =  get_next_line(fd);
-    printf("%s", line);
-    if (line)
-        free(line);
-    line =  get_next_line(fd);
-    printf("%s", line);
-    if (line)
-        free(line);
-    close(fd);
+    if (ntest == passed)
+    {
+        printf(" - OK");
+        testpassed++;
+    }
+    else
+        printf(" - KO");
     return (1);
 }
 
@@ -142,26 +200,95 @@ static int test1()
 {
     int		fd;
     char    *line;
+    int     ntest = 0;
+    int     passed = 0;
 
-    printf("\n> test 1\n");
+    nb_test++;
+    printf("\n> test 1");
 
     fd = open("test1.txt", 00);
 
     if (fd < 0)
         return (printf("Error opening file!\n"), -1);
+
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "abcdefghijklmnopqrstuvwxyz\n");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+    // printf("%s", line);
     if (line)
         free(line);
+
+    ntest++;
     line =  get_next_line(fd);
-    printf("%s", line);
+    passed += !strcmp(line, "0123456789");
+    // printf("%s", line);
     if (line)
         free(line);
     close(fd);
+
+    if (ntest == passed)
+    {
+        printf(" - OK");
+        testpassed++;
+    }
+    else
+        printf(" - KO");
+    return (1);
+}
+
+static int test2()
+{
+    int		fd;
+    char    *line;
+    int     ntest = 0;
+    int     passed = 0;
+
+    nb_test++;
+    printf("\n> test 2");
+
+    fd = open("test2.txt", 00);
+
+    if (fd < 0)
+        return (printf("Error opening file!\n"), -1);
+
+
+    ntest++;
+    line =  get_next_line(fd);
+    passed += !strcmp(line, "(null)");
+    // printf("%s", line);
+    if (line)
+        free(line);
+
+    ntest++;
+    line =  get_next_line(fd);
+    passed += !strcmp(line, "(null)\n");
+    // printf("%s", line);
+    if (line)
+        free(line);
+    
+    ntest++;
+    line =  get_next_line(fd);
+    passed += !strcmp(line, "(null)\n");
+    // printf("%s", line);
+    if (line)
+        free(line);
+    close(fd);
+
+    if (ntest == passed)
+    {
+        printf(" - OK");
+        testpassed++;
+    }
+    else
+        printf(" - KO");
     return (1);
 }
 
@@ -174,6 +301,11 @@ int main(void)
     test4();
     test5();
     test6();
+
+    if (nb_test == testpassed)
+        printf("\nAll tests passed :-) (passed: %d/%d)\n", testpassed, nb_test);
+    else
+        printf("\nSome tests failed :/ (passed: %d/%d)\n", testpassed, nb_test);
 
     return (0);
 }
