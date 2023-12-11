d5fc7b733f613c4d7ec36085c350fa9144159039
Steven Bandaogo <sbandaog@student.42.fr>
2023-12-11 15:51:17 +0100

    corr stdin bash

--- test_stdin.sh
+++ test_stdin.sh
@@ -1,2 +1,2 @@
-cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=20 ../get_next_line_bonus.c ../get_next_line_utils_bonus.c test_stdin.c -o tests -I../
-echo -e -n "This is line 1\nThis is line 2\nThis is line 3" | ./tests
+cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=20 ../get_next_line.c ../get_next_line_utils.c test_stdin.c -o tests -I../
+echo -e -n "This is line 1\nThis is line 2\nThis is line 3" | valgrind ./tests
