ad09b2a31a4a16a0ef41c1310254300137f97dba
Steven Bandaogo <sbandaog@student.42.fr>
2023-12-14 21:02:12 +0100

    minor changes buffer size

--- test_bonus.sh
+++ test_bonus.sh
@@ -1,2 +1,2 @@
-cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=20 ../get_next_line_bonus.c ../get_next_line_utils_bonus.c test_bonus.c -o tests -I../
+cc -Wall -Wextra -Werror -g -D BUFFER_SIZE=10 ../get_next_line_bonus.c ../get_next_line_utils_bonus.c test_bonus.c -o tests -I../
 valgrind ./tests
